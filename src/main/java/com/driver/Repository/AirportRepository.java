18b38786184cc0c275da0ab893db314d4159cad7
pranav <pranavanandth19@gamil.com>
2023-09-04 11:31:07 +0530

    1st try

---
+++ src/main/java/com/driver/Repository/AirportRepository.java
@@ -0,0 +1,87 @@
+package com.driver.Repository;
+
+import com.driver.model.Airport;
+import com.driver.model.City;
+import com.driver.model.Flight;
+import com.driver.model.Passenger;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+@Repository
+public class AirportRepository {
+    HashMap<String, Airport> airports =new HashMap<>();
+    HashMap<Integer, Flight> flights=new HashMap<>();
+    HashMap<Integer, Passenger> passengers=new HashMap<>();
+    HashMap<Integer, ArrayList<Integer>> flightTicketsBooked = new HashMap<>(); //<flight id, its passengers id list>
+    HashMap<Integer,ArrayList<Integer> > usersBooking=new HashMap<>(); //<passengerid , list of booked flight ids>
+    public HashMap<Integer, ArrayList<Integer>> getFlightTicketsBooked()
+    {
+        return flightTicketsBooked;
+    }
+    public HashMap<Integer,ArrayList<Integer>> getUsersBooking()
+    {
+        return usersBooking;
+    }
+    public HashMap<Integer, Flight> getFlights() {
+        return flights;
+    }
+    public HashMap<String,Airport> getairports()
+    {
+        return airports;
+    }
+
+    public void addAirport(String airportName, Airport airport) {
+        airports.put(airportName,airport);
+    }
+
+
+    public String getLargestAirportName() {
+        int maxt=0; //maxterminal
+        String maxn=null; //max airport name
+
+        for(String i: airports.keySet())
+        {
+            int curt= airports.get(i).getNoOfTerminals();
+            if(curt > maxt)
+            {
+                maxt=curt;
+                maxn=i;
+            }
+            else if(curt==maxt)
+            {
+                if(maxn.compareTo(i)<0)
+                    maxn=i;
+            }
+        }
+
+        return maxn;
+    }
+
+    public double getShortestDurationOfPossibleBetweenTwoCities(City fromCity, City toCity) {
+        double mind=Double.MAX_VALUE;
+
+        for(int i: flights.keySet())
+        {
+            if(flights.get(i).getFromCity()==fromCity && flights.get(i).getToCity()==toCity)
+            {
+                mind=Math.min(mind,flights.get(i).getDuration());
+            }
+        }
+        if(mind==Double.MAX_VALUE)
+            return -1;
+
+        return mind;
+    }
+
+    public void addFlight(Flight flight) {
+        flights.put(flight.getFlightId(),flight);
+    }
+
+    public void addPassenger(Passenger passenger) {
+        passengers.put(passenger.getPassengerId(), passenger);
+    }
+
+
+}
---
+++ src/main/java/com/driver/service/AirportService.java
@@ -0,0 +1,181 @@
+package com.driver.service;
+
+import com.driver.Repository.AirportRepository;
+import com.driver.model.Airport;
+import com.driver.model.City;
+import com.driver.model.Flight;
+import com.driver.model.Passenger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.*;
+
+@Service
+public class AirportService {
+
+    @Autowired
+    AirportRepository repository;
+    public void addAirport(Airport airport) {
+        repository.addAirport(airport.getAirportName(),airport);
+    }
+
+    public String getLargestAirportName() {
+        return repository.getLargestAirportName();
+    }
+
+    public double getShortestDurationOfPossibleBetweenTwoCities(City fromCity, City toCity) {
+        return repository.getShortestDurationOfPossibleBetweenTwoCities(fromCity, toCity);
+    }
+
+    public String addFlight(Flight flight) {
+        repository.addFlight(flight);
+        return "SUCCESS";
+    }
+
+    public String addPassenger(Passenger passenger) {
+        repository.addPassenger(passenger);
+        return "SUCCESS";
+    }
+
+    public String bookATicket(Integer flightId, Integer passengerId) {
+        HashMap<Integer,ArrayList<Integer>> flightTicketsBooked = repository.getFlightTicketsBooked();
+        HashMap<Integer,Flight> flights =repository.getFlights();//to get flight's maxcapacity
+        HashMap<Integer,ArrayList<Integer>> usersBooking =repository.getUsersBooking();
+
+        ArrayList<Integer> bookedList =flightTicketsBooked.getOrDefault(flightId,new ArrayList<>());
+
+        int noOfBooked=bookedList.size();
+        int fcapacity=flights.get(flightId).getMaxCapacity();
+
+        if( !flights.containsKey(flightId) ||  noOfBooked>=fcapacity || (usersBooking.containsKey(passengerId) && usersBooking.get(passengerId).contains(flightId) ) )
+        {
+
+            return "FAILURE";
+        }
+
+        //......
+        if(noOfBooked==0)
+        {
+            flightTicketsBooked.put(flightId,new ArrayList<>());
+        }
+        if(!usersBooking.containsKey(passengerId) )
+        {
+            usersBooking.put(passengerId,new ArrayList<>());
+        }
+
+        flightTicketsBooked.get(flightId).add(passengerId);
+        usersBooking.get(passengerId).add((flightId));
+
+        return "SUCCESS";
+
+
+
+    }
+
+    public String getAirportNameFromFlightId(Integer flightId) {
+
+        HashMap<Integer,Flight> flights =repository.getFlights();
+        HashMap<String , Airport> airports= repository.getairports();
+
+        String fromCity= flights.get(flightId).getFromCity().toString();
+
+        for(String i: airports.keySet())
+        {
+            String airportCity=airports.get(i).getCity().toString();
+            if( fromCity.equals(airportCity) )
+            {
+                return airports.get(i).getAirportName();
+            }
+        }
+
+        return null;
+    }
+
+    public String cancelATicket(Integer flightId, Integer passengerId) {
+
+        //If the passenger has not booked a ticket for that flight or the flightId is invalid or in any other failure case
+        // then return a "FAILURE" message
+        // Otherwise return a "SUCCESS" message
+        // and also cancel the ticket that passenger had booked earlier on the given flightId
+
+        HashMap<Integer,ArrayList<Integer>> usersBooking= repository.getUsersBooking();
+        HashMap<Integer,ArrayList<Integer>> flightTicketsBooked= repository.getFlightTicketsBooked();
+
+        if( !(flightTicketsBooked.containsKey(flightId)) || !(usersBooking.containsKey(passengerId)) || !( usersBooking.get(passengerId).contains(flightId) ) )
+        {
+            return "FAILURE";
+        }
+
+        flightTicketsBooked.get(flightId).remove(passengerId);
+        usersBooking.get(passengerId).remove((flightId));
+
+        return "SUCCESS";
+    }
+
+    public int countOfBookingsDoneByPassengerAllCombined(Integer passengerId) {
+        return repository.getUsersBooking().get(passengerId).size();
+    }
+
+    public int calculateFlightFare(Integer flightId) {
+        //Calculation of flight prices is a function of number of people who have booked the flight already.
+        //Price for any flight will be : 3000 + noOfPeopleWhoHaveAlreadyBooked*50
+        //Suppose if 2 people have booked the flight already : the price of flight for the third person will be 3000 + 2*50 = 3100
+        //This will not include the current person who is trying to book, he might also be just checking price
+
+        HashMap<Integer,ArrayList<Integer>> flightTicketsBooked= repository.getFlightTicketsBooked();
+
+        //1st ticket of that flight (none booked the flight)
+        if(!flightTicketsBooked.containsKey(flightId) )
+        {
+            return 3000;
+        }
+
+        int count=flightTicketsBooked.get(flightId).size();
+
+        return 3000 + (count*50);
+
+
+    }
+
+    public int calculateRevenueOfAFlight(Integer flightId) {
+        HashMap<Integer,ArrayList<Integer>> flightTicketsBooked= repository.getFlightTicketsBooked();
+
+        int revenue=0;
+        //none booked
+        if( !flightTicketsBooked.containsKey(flightId) )
+            return revenue;
+
+        int size=flightTicketsBooked.get(flightId).size();
+
+        for(int i=0;i<size;i++)
+        {
+            revenue+= (3000 + (i*50) ); //adding actual cost of every passengers of flight
+        }
+
+        return revenue;
+    }
+
+    public int getNumberOfPeopleOn(Date date, String airportName) {
+        HashMap<String,Airport> airports = repository.getairports();
+        HashMap<Integer,Flight> flights= repository.getFlights();
+        HashMap<Integer,ArrayList<Integer>> flightTicketsBooked = repository.getFlightTicketsBooked();
+
+        int count=0;
+
+        for(Integer i:flights.keySet())
+        {
+            Date fDate=flights.get(i).getFlightDate();
+
+            if( fDate.equals(date) )
+            {
+                if(flights.get(i).getFromCity().equals(airportName) || flights.get(i).getToCity().equals(airportName) )
+                {
+                    if(flightTicketsBooked.containsKey(i))
+                    {
+                        count+=flightTicketsBooked.get(i).size();
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
